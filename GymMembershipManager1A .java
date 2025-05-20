6397157648d8688b1000e9b3b9e01fcb029ccaf1
Anuj <anujsrinivas00@gmail.com>
2025-05-20 06:59:49 +0530

    Update and rename prgm1a.java to GymMembershipManager1A .java

--- GymMembershipManager1A .java
+++ GymMembershipManager1A .java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class GymMembershipManager {
+public class GymMembershipManager1A {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter member name: ");
